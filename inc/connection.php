0ad5c52b534c38f99fffba9b828ac3bf8598b4b1
isuru <isurunuwanthilaka@gmail.com>
2018-03-22 16:43:19 +0530

    test

--- inc/connection.php
+++ inc/connection.php
@@ -4,9 +4,9 @@
 	$dbuser = 'xpigpcxaxfdfwh';
 	$dbpass = '41219beab15ee40a8862f19f1722c473128935f04b7c9daa41290440d6ca8b48';
 	$dbname = 'dcu7j31bguguik';
-	//$dbport = 5432;
+	$dbport = 5432;
 
-	$connection = mysqli_connect($dbhost,$dbuser,$dbpass, $dbname);
+	$connection = mysqli_connect($dbhost,$dbuser,$dbpass, $dbname,$dbport);
 
 	if (mysqli_connect_errno()) {
 		die('Database connection failed'.mysqli_connect_error());
