2e51fb5638f56e2ee81e4bf889fc54c6d0950be2
isuru <isurunuwanthilaka@gmail.com>
2018-03-23 14:04:45 +0530

    sign up page update again

--- sign-up.php
+++ sign-up.php
@@ -1,8 +1,13 @@
 <?php session_start();?>
-<?php require_once('inc/connection.php'); ?>
+<?php require_once('inc/connection.php');
+?>
 <?php 
 $errors=array();
  ?>
+
+
+
+
 
 <!DOCTYPE html>
 <html lang="en">
@@ -14,32 +19,53 @@ $errors=array();
 <body>
 	<header>
 		<div class="appname"> User Management System</div>
-		<div class="sign-up"> Welcome user!<a href="index.php">Sign in</a></div>
+		<div class="loggedin"> Welcome user!<a href="index.php">Sign in</a></div>
 	</header>
 	<main>
 		<div class="adduser">
-			<form action="add-user.php" method="post" class="userform">
+			<form action="sign-up.php" method="post" class="userform">
 			
 				<p >
 					<label for="">First Name</label>
-					<input type="text" name="first_name_var" id="" required>
+					<input type="text" name="first_name_var" id="" <?php if (isset($_POST['first_name_var'])) {
+						echo "value = {$_POST['first_name_var']}";
+					}?> required>
 				</p>
 				<p >
 					<label for="">Last Name</label>
-					<input type="text" name="last_name_var" id="" required>
+					<input type="text" name="last_name_var" id="" <?php if (isset($_POST['last_name_var'])) {
+						echo "value = {$_POST['last_name_var']}";
+					}?> required>
 				</p>
 				<p >
 					<label for="">Email Address</label>
-					<input type="text" name="email_var" id="" required>
+					<input type="text" name="email_var" id="" <?php if (isset($_POST['email_var'])) {
+						echo "value = {$_POST['email_var']}";
+					}?> required>
 				</p>
 				<p >
 					<label for="">Password</label>
-					<input type="password" name="password_var" id="" required>
+					<input type="password" name="password_var" id="" <?php if (isset($_POST['password_var'])) {
+						echo "value = {$_POST['password_var']}";
+					}?> required>
+				</p>
+				<p >
+					<label for="">Confirm Password</label>
+					<input type="password" name="confirm_password_var" id="" <?php if (isset($_POST['confirm_password_var'])) {
+						echo "value = {$_POST['confirm_password_var']}";
+					}?> required>
 				</p>
 				<p>
 					<label for="">&nbsp;</nav></label>
-					<button type="submit" name="save"> Sign up</button>
+					<button type="submit" name="requestpin"> Request Pin Number</button>
 				</p>
+				
+				<?php if (isset($_POST['requestpin'])) {
+					echo "<p class = 'pinmsg'> Check your email box for the PIN </p>";
+				} ?>
+			
+
+
 
 				
 			</form>
@@ -49,3 +75,4 @@ $errors=array();
 </body>
 </html>
 <?php pg_close($connection);?>
+
