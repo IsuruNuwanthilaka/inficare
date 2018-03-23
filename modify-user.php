1358f6bec608194e0921f9477431d3e5ff59a735
isuru <isurunuwanthilaka@gmail.com>
2018-03-23 21:52:47 +0530

    test database

---
+++ modify-user.php
@@ -0,0 +1,15 @@
+<?php session_start();?>
+<?php require_once('inc/connection.php'); ?>
+
+<?php 
+
+	$query = "INSERT INTO itemdb(item_description,is_request,is_donated) VALUES('10 books',0,0)";
+
+	$result_set = pg_query($connection,$query);
+
+	if (!$result_set) {
+		die('Error');
+	}
+ ?>
+
+<?php pg_close($connection); ?>
