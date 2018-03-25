9eb3c468fa3dbf7776b94235b779b8262051110d
isuru <isurunuwanthilaka@gmail.com>
2018-03-25 23:49:21 +0530

    change

--- sign-up.php
+++ sign-up.php
@@ -79,12 +79,12 @@ $errors=array();
 					if($another_result){
 						
 						if (pg_num_rows($another_result)==0) {
-							$query = 'INSERT INTO userdb(first_name,last_name,email,password,last_login,is_deleted) VALUES (';
-							$query .= $first_name.',';
-							$query .= $last_name.',';
-							$query .= $email.',';
-							$query .= $password.',';
-							$query .='0000-00-00 00:00:00'.',';
+							$query = 'INSERT INTO userdb(first_name,last_name,email,password,last_login,is_deleted) VALUES (\'';
+							$query .= $first_name.'\',\'';
+							$query .= $last_name.'\',\'';
+							$query .= $email.'\',\'';
+							$query .= $password.'\',\'';
+							$query .='0000-00-00 00:00:00'.'\',\'';
 							$query .= '0)';
 							echo $query;
 							$result = pg_query($connection,$query);
