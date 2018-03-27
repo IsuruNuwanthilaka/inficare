d343144d0c45d956656a61a90ac818485011ae46
isuru <isurunuwanthilaka@gmail.com>
2018-03-27 18:51:51 +0530

    change

---
+++ inc/functions.php
@@ -0,0 +1,29 @@
+<?php 
+	$mail             = new PHPMailer();
+
+	$mail->IsSMTP();
+	$mail->SMTPDebug  = 2;                     // enables SMTP debug information (for testing)
+	                                           // 1 = errors and messages
+	                                           // 2 = messages only
+	$mail->SMTPAuth   = true;                  // enable SMTP authentication
+	$mail->SMTPSecure = "tls";                 
+	$mail->Host       = "smtp.gmail.com";      // SMTP server
+	$mail->Port       = 587;                   // SMTP port
+	$mail->Username   = "isumalabs@gmail.com";  // username
+	$mail->Password   = "Isuma@U0M";            // password
+
+	$mail->SetFrom('isumalabs@gmail.com', 'Test');
+
+	$mail->Subject    = "I hope this works!";
+
+	$mail->MsgHTML('Blah');
+
+	$address = "isurunuwanthilaka@gmail.com";
+	$mail->AddAddress($address, "Test");
+
+	if(!$mail->Send()) {
+	  echo "Mailer Error: " . $mail->ErrorInfo;
+	} else {
+	  echo "Message sent!";
+	}
+ ?>
