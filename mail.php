30b293bcbc5c2f7b8f603f6b13157cf7aaab9af8
isuru <isurunuwanthilaka@gmail.com>
2018-03-27 23:37:01 +0530

    change

--- mail.php
+++ mail.php
@@ -1,38 +1,27 @@
-<?php 
-include("inc/PHPMailer.php"); 
-include("inc/SMTP.php"); 
-use PHPMailer\PHPMailer\PHPMailer;
+<?php
 
-	$mail = new PHPMailer;
+  require("/inc/PHPMailer.php");
+  require("/inc/SMTP.php");
 
-	$mail->isSMTP();
-	$mail->SMTPDebug  = 1;
-	$mail->SMTPAuth   = true;
-	$mail->SMTPSecure = "tls";                 
-	$mail->Host       = "smtp.gmail.com";
-	$mail->Port       = 587; 
-	$mail->isHTML(true);
-	$mail->Username   = "infoatsoulmate@gmail.com";
-	$mail->Password   = "950500085v";
+    $mail = new PHPMailer\PHPMailer\PHPMailer();
+    $mail->isSMTP();
 
-	$mail->setFrom('infoatsoulmate@gmail.com');
+    $mail->SMTPDebug = 1;
+    $mail->SMTPAuth = true;
+    $mail->SMTPSecure = 'ssl';
+    $mail->Host = "smtp.gmail.com";
+    $mail->Port = 465; // or 587
+    $mail->isHTML(true);
+    $mail->Username = "infoatsoulmate@gmail.com";
+    $mail->Password = "950500085v";
+    $mail->setFrom("infoatsoulmate@gmail.com");
+    $mail->Subject = "Test";
+    $mail->Body = "hello";
+    $mail->addAddress("isurunuwanthilaka@gmail.com");
 
-	$mail->Subject = "I hope this works!";
-
-	$mail->Body = "Hello"
-
-	$address = "isurunuwanthilaka@gmail.com";
-	$mail->addAddress($address);
-
-	if(!$mail->send()) {
-	  echo "Mailer Error: " . $mail->ErrorInfo;
-	} else {
-	  echo "Message sent!";
-	}
-
-
-
-
-echo "end process";
-
- ?>
+     if(!$mail->send()) {
+        echo "Mailer Error: " . $mail->ErrorInfo;
+     } else {
+        echo "Message has been sent";
+     }
+?>
