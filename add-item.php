e6f33b5df4c9f8a89c215773a9a2ec87a82de009
isuru <isurunuwanthilaka@gmail.com>
2018-03-27 16:42:26 +0530

    change

--- add-item.php
+++ add-item.php
@@ -25,11 +25,6 @@ $errors=array();
 	</header>
 	<main>
 		<h1>Add New Item<span><a href="home.php">< Back to Item List</a></span></h1>
-		<?php 
-			if (!empty($errors)) {
-			echo $errors[0];
-		}
-		 ?>
 		<div class="additem">
 			<form action="add-item.php" method="post" class="itemform">
 			
@@ -56,11 +51,10 @@ $errors=array();
 				<?php 
 				$is_request = 0;
 				if (isset($_POST['save'])) {
-					$is_request =$_POST['status'];
+					$is_request =(int)$_POST['status'];
 					$item_name = pq_escape_string($connection,$_POST['item_name']);
 					$item_description = pq_escape_string($connection,$_POST['item_description']);
 					$item_email = $_SESSION['email'];
-
 					$query = 'INSERT INTO itemdb (item_description,item_name,item_email,is_request,is_donated) VALUES (\'';
 					$query .= $item_description.'\',\'';
 					$query .= $item_name.'\',\'';
@@ -68,6 +62,7 @@ $errors=array();
 					$query .= $is_request.',';
 					$query .= '2)';
 					echo "<p class = 'errormsg'>{$query}</p>";
+					
 					$result = pg_query($connection,$query);
 
 					if ($result) {
