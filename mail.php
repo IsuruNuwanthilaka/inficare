a8b8f5d74d603d11d8e32ed4ed6d0e0aebc353c0
isuru <isurunuwanthilaka@gmail.com>
2018-03-28 12:04:09 +0530

    change

--- mail.php
+++ mail.php
@@ -4,6 +4,6 @@ require_once("inc/functions.php");
 
 $address = 'isurunuwanthilaka@gmail.com';
 $subject = 'Welcome';
-$body = 'Hi user!';
+$body = 'Hi another user!';
 sendMail($subject,$body,$address);
 ?>
