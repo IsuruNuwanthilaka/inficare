3bfbf03566370854029b5d3f7cce8d6fd292abfd
isuru <isurunuwanthilaka@gmail.com>
2018-03-28 11:30:13 +0530

    change

--- inc/functions.php
+++ inc/functions.php
@@ -1,3 +1,32 @@
-<?php 
-	require_once('PHPMailer.php');
+<?php
+
+	function sendMail($subject,$body,$address){
+		use PHPMailer\PHPMailer\PHPMailer;
+		use PHPMailer\PHPMailer\Exception;
+	
+	  	require_once("inc/PHPMailer.php");
+	 	require_once("inc/SMTP.php");
+	
+	    $mail = new PHPMailer;
+	
+	    $mail->isSMTP();
+	
+	    $mail->SMTPDebug = false;
+	    $mail->SMTPAuth = true;
+	    $mail->SMTPAutoTLS = false;
+	    $mail->SMTPSecure = 'tls';
+	    $mail->Host = gethostbyname('tls://smtp.gmail.com');
+	    $mail->Port = 587;
+	    $mail->isHTML(true);
+	    $mail->Username = "info.isumacare@gmail.com";
+	    $mail->Password = "950500085v";
+	    $mail->setFrom("info.isumacare@gmail.com");
+	    $mail->Subject = $subject;
+	    $mail->Body = $body;
+	    $mail->addAddress($address);
+	
+	     if(!$mail->send()) {
+	        echo "Mailer Error: " . $mail->ErrorInfo;
+	     }
+	 }
  ?>
--- mail.php
+++ mail.php
@@ -1,31 +1,8 @@
 <?php
-	use PHPMailer\PHPMailer\PHPMailer;
-	use PHPMailer\PHPMailer\Exception;
-
-  	require_once("inc/PHPMailer.php");
- 	require_once("inc/SMTP.php");
-
-    $mail = new PHPMailer;
-
-    $mail->isSMTP();
-
-    $mail->SMTPDebug = false;
-    $mail->SMTPAuth = true;
-    $mail->SMTPAutoTLS = false;
-    $mail->SMTPSecure = 'tls';
-    $mail->Host = gethostbyname('tls://smtp.gmail.com');
-    $mail->Port = 587;
-    $mail->isHTML(true);
-    $mail->Username = "info.isumacare@gmail.com";
-    $mail->Password = "950500085v";
-    $mail->setFrom("info.isumacare@gmail.com");
-    $mail->Subject = "Test";
-    $mail->Body = "hello";
-    $mail->addAddress("isurunuwanthilaka@gmail.com");
-
-     if(!$mail->send()) {
-        echo "Mailer Error: " . $mail->ErrorInfo;
-     } else {
-        echo "Message has been sent";
-     }
+    require_once('inc/functions.php');
+	$address = 'isurunuwanthilaka@gmail.com';
+    $subject = 'Welcome to ISUMACARE donation management system!';
+    $body = 'Hi! 
+             We are very happy to see you in our system.';
+    sendMail($subject,$body,$address);
 ?>
