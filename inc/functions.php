33c6367315ca86b15a85a4cd347f440a2f831a21
isuru <isurunuwanthilaka@gmail.com>
2018-03-28 11:40:42 +0530

    change

--- inc/functions.php
+++ inc/functions.php
@@ -1,35 +1,3 @@
-<?php
-
-	function sendMail($subject,$body,$address){
-		use PHPMailer\PHPMailer\PHPMailer;
-		use PHPMailer\PHPMailer\Exception;
-	
-	  	require_once("inc/PHPMailer.php");
-	 	require_once("inc/SMTP.php");
-	
-	    $mail = new PHPMailer;
-	
-	    $mail->isSMTP();
-	
-	    $mail->SMTPDebug = false;
-	    $mail->SMTPAuth = true;
-	    $mail->SMTPAutoTLS = false;
-	    $mail->SMTPSecure = 'tls';
-	    $mail->Host = gethostbyname('tls://smtp.gmail.com');
-	    $mail->Port = 587;
-	    $mail->isHTML(true);
-	    $mail->Username = "info.isumacare@gmail.com";
-	    $mail->Password = "950500085v";
-	    $mail->setFrom("info.isumacare@gmail.com");
-	    $mail->Subject = $subject;
-	    $mail->Body = $body;
-	    $mail->addAddress($address);
-	
-	     if(!$mail->send()) {
-	        echo "Mailer Error: " . $mail->ErrorInfo;
-	     }else{
-	     	echo "Successfully sent mail.";
-	     }
-
-	 }
+<?php 
+	require_once('PHPMailer.php');
  ?>
