2dfe5c29bb995b45595fee9fb32e34eb59e652e6
isuru <isurunuwanthilaka@gmail.com>
2018-03-28 11:53:22 +0530

    change

--- inc/functions.php
+++ inc/functions.php
@@ -1,3 +1,24 @@
 <?php 
-	require_once('PHPMailer.php');
+function sendMail($subject,$body,$address){
+	//sending emails
+    $mail = new PHPMailer;
+    $mail->isSMTP();
+    $mail->SMTPDebug = false;
+    $mail->SMTPAuth = true;
+    $mail->SMTPAutoTLS = false;
+    $mail->SMTPSecure = 'tls';
+    $mail->Host = gethostbyname('tls://smtp.gmail.com');
+    $mail->Port = 587;
+    $mail->isHTML(true);
+    $mail->Username = "info.isumacare@gmail.com";
+    $mail->Password = "950500085v";
+    $mail->setFrom("info.isumacare@gmail.com");
+    $mail->Subject = $subject;
+    $mail->Body = $body;
+    $mail->addAddress($address);
+
+     if(!$mail->send()) {
+        echo "Mailer Error: " . $mail->ErrorInfo;
+     }
+}
  ?>
--- mail.php
+++ mail.php
@@ -4,34 +4,10 @@ use PHPMailer\PHPMailer\Exception;
 
 require_once("inc/PHPMailer.php");
 require_once("inc/SMTP.php");
+require_once("inc/functions.php");
 
-function sendMail(){
-	
-
-    $mail = new PHPMailer;
-
-    $mail->isSMTP();
-
-    $mail->SMTPDebug = false;
-    $mail->SMTPAuth = true;
-    $mail->SMTPAutoTLS = false;
-    $mail->SMTPSecure = 'tls';
-    $mail->Host = gethostbyname('tls://smtp.gmail.com');
-    $mail->Port = 587;
-    $mail->isHTML(true);
-    $mail->Username = "info.isumacare@gmail.com";
-    $mail->Password = "950500085v";
-    $mail->setFrom("info.isumacare@gmail.com");
-    $mail->Subject = "Test";
-    $mail->Body = "hello";
-    $mail->addAddress("isurunuwanthilaka@gmail.com");
-
-     if(!$mail->send()) {
-        echo "Mailer Error: " . $mail->ErrorInfo;
-     } else {
-        echo "Message has been sent";
-     }
-}
-
-sendMail();
+$address = 'isurunuwanthilaka@gmail.com';
+$subject = 'Welcome'
+$body = 'Hi user!';
+sendMail($subject,$body,$address);
 ?>
