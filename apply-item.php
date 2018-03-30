aa2a110d0c63dfb5ccebe0331f6aa47e836c7e00
isuru <isurunuwanthilaka@gmail.com>
2018-03-30 11:53:24 +0530

    change

--- apply-item.php
+++ apply-item.php
@@ -8,7 +8,7 @@ require_once('inc/functions.php');
 	$item_id=$_GET['item_id'];	
 	$query = "SELECT * FROM itemdb WHERE item_id =";
 	$query .= $item_id;
-	$query .= " LIMIT 1"
+	$query .= " LIMIT 1";
 	$result_set = pg_query($connection, $query);
 	if ($result_set) {
 		$item = pg_fetch_assoc($result_set);
